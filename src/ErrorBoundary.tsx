82c0e8672e8463bf9bed57ad884676a9c6733ba1
Serdar Özdemir <soezdemir3423@gmail.com>
2023-02-12 09:14:19 +0100

    Create ErrorBoundary
    Created ErrorBoundary

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import React, { ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {}
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <h1>Something went wrong</h1>;
+    }
+    return this.props.children;
+  }
+}
