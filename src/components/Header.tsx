679518b228e9d4aee225a67eb9e6a9a06b780ba7
Serdar Özdemir <soezdemir3423@gmail.com>
2023-02-26 18:23:01 +0100

    include Typography component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { Typography } from "@material-tailwind/react";
+
 export default function Header() {
   // we need to get the days
 
   return (
     <header className="bg-blue-gray-200 p-4">
-      <h1>Weatherlicious</h1>
+      <Typography variant="h1">Weatherlicious</Typography>
     </header>
   );
 }
