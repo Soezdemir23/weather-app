4af01d2970ef94c6dc0007a174b0c38ac46ef154
Serdar Özdemir <soezdemir3423@gmail.com>
2023-02-28 21:32:00 +0100

    add reactQuerytools and enabled it

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react/context/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient} contextSharing={true}>
         <App />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
