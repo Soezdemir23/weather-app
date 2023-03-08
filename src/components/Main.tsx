c6d08ccaea0cf037a77a08065ba63b15fd75631c
Serdar Özdemir <soezdemir3423@gmail.com>
2023-03-08 23:39:45 +0100

    Update Main.tsx
    + refactored props types
    + implemented a function that returns the day
    + implemented function to traverse through a week and assign weather
    + conditional in case weather data is missing
    + refactored jsx according to new functions and correct interface typing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,172 +5,112 @@ import {
   CardFooter,
   Typography,
 } from "@material-tailwind/react";
+import { apiCall2CustomObjects } from "../interfaces";
 
 import { WMOInterpreter, WMOComponent } from "../wmoUtil";
-import MaterialSymbolsFavoriteOutlineSharp from "~icons/material-symbols/favorite-outline-sharp";
-import MaterialSymbolsFavoriteSharp from "~icons/material-symbols/favorite-sharp";
 // first an interface for just one object.
 
 interface CurrentWeatherObject {
-  weather: { [key: string]: any } | undefined;
+  weather: apiCall2CustomObjects.Weather;
+  results: { [key: string]: any }[];
 }
 
-export default function Main({ weather }: CurrentWeatherObject) {
-  if (weather === undefined) {
+export default function Main({ weather, results }: CurrentWeatherObject) {
+  // use a let first. Later go for a Ref. Maybe use state if you necessarily need to re-render it.
+  // I can't test the object, stupid read write. Might as well pass it to this main after I have put it all into a query run and return it.
+
+  // There is a problem with the daily weather being rendered automatically due to constrains of the JSON properties etc.
+  // , which would make life easier
+  function indexToDayName(number: number): string {
+    switch (number) {
+      case 0:
+        return "Mon";
+      case 1:
+        return "Tue";
+      case 2:
+        return "Wed";
+      case 3:
+        return "Thu";
+      case 4:
+        return "Fri";
+      case 5:
+        return "Sat";
+      case 6:
+        return "Sun";
+      default:
+        return "Unknown day " + number;
+    }
+  }
+
+  function returnTheWeek(
+    theWeather: apiCall2CustomObjects.Weather
+  ): React.ReactElement[] | React.ReactElement {
+    let theWeek: React.ReactElement[] = [];
+    if (
+      theWeather.dailiesWeather === undefined ||
+      theWeather.dailiesWeather.weathercode === undefined ||
+      theWeather.dailiesWeather.temperature_2m_max === undefined ||
+      theWeather.dailiesWeather.temperature_2m_min === undefined
+    )
+      return <p>Problem with the passed object, weather unavailable</p>;
+    for (
+      let index = 0;
+      index < theWeather.dailiesWeather?.weathercode?.length;
+      index++
+    ) {
+      theWeek.push(
+        <div key={index}>
+          <Typography variant={"small"}>
+            {" "}
+            {WMOComponent(theWeather.dailiesWeather.weathercode[index])}
+          </Typography>
+          <Typography variant={"small"}>
+            {(
+              (theWeather.dailiesWeather.temperature_2m_max[index] +
+                theWeather.dailiesWeather.temperature_2m_min[index]) /
+              2
+            ).toFixed(1)}{" "}
+            °C
+          </Typography>
+          <Typography variant={"small"}>{indexToDayName(index)}</Typography>
+        </div>
+      );
+    }
+    return theWeek;
+  }
+
+  if (weather === undefined || weather === null) {
     return (
       <>
-        <h1>...Loading</h1>
-
-        <h1>...Loading</h1>
-        <h1>...Loading</h1>
-        <h1>...Loading</h1>
-        <h1>...Loading</h1>
-        <h1>...Loading</h1>
+        <h1>...weather data is missing or mangled</h1>
       </>
     );
   }
-  console.log(weather);
+
   return (
-    <main className="flex-1 mx-10 ">
+    <main className="flex-1 mx-10">
       <div className="grid pt-7 grid-row-3 grid-cols-4">
         <Card>
           <CardHeader color="blue-gray" className="text-center">
             <Typography variant="h5" className="mb-2 flex justify-center">
-              {weather.fetchLongLat[0].name === undefined
-                ? "not there"
-                : weather.fetchLongLat[0].name}
+              {weather.cityname}
             </Typography>
-            <Typography variant="h5">
-              {weather.fetchLongLat[0].country}
-            </Typography>
-
             <Typography>
-              temperature:{" "}
-              {weather.processWeekWeather.current_weather.temperature}C°
+              {weather.state + ", " + weather.countrycode}
             </Typography>
-
             <Typography>
-              weather:{" "}
-              {WMOInterpreter(
-                weather.processWeekWeather.current_weather.weathercode
-              )}{" "}
-              {WMOComponent(
-                weather.processWeekWeather.current_weather.weathercode
-              )}
+              temperature: {weather.currentWeather.temperature} °C
+            </Typography>
+            <Typography>
+              weather: {WMOInterpreter(weather.currentWeather.weathercode)}
+              {WMOComponent(weather.currentWeather.weathercode)}
             </Typography>
           </CardHeader>
-          <CardBody color="orange" className=" flex justify-center gap-2 ">
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[0])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[0] +
-                    weather.processWeekWeather.daily.temperature_2m_min[0]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Mon</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[1])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[1] +
-                    weather.processWeekWeather.daily.temperature_2m_min[1]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Tue</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[2])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[2] +
-                    weather.processWeekWeather.daily.temperature_2m_min[2]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Wed</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[3])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[3] +
-                    weather.processWeekWeather.daily.temperature_2m_min[3]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Thu</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[4])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[4] +
-                    weather.processWeekWeather.daily.temperature_2m_min[4]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Fri</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[5])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[5] +
-                    weather.processWeekWeather.daily.temperature_2m_min[5]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Sat</Typography>
-            </div>
-            <div>
-              <Typography variant={"small"}>
-                {WMOComponent(weather.processWeekWeather.daily.weathercode[6])}
-              </Typography>
-              <Typography variant={"small"}>
-                {(
-                  (weather.processWeekWeather.daily.temperature_2m_max[6] +
-                    weather.processWeekWeather.daily.temperature_2m_min[6]) /
-                  2
-                ).toFixed(1)}{" "}
-                °C
-              </Typography>
-              <Typography variant={"small"}>Sun</Typography>
-            </div>
+          <CardBody className="flex justify-center gap-2">
+            {returnTheWeek(weather)}
           </CardBody>
-          <CardFooter>
-            <span>
-              Favorite:{" "}
-              {
-                <>
-                  <MaterialSymbolsFavoriteOutlineSharp />
-                  <MaterialSymbolsFavoriteSharp />
-                </>
-              }
-            </span>
-          </CardFooter>
-        </Card>{" "}
+          <CardFooter>asdf</CardFooter>
+        </Card>
       </div>
     </main>
   );
