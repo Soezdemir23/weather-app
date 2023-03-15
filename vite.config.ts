d6cbaf38bd1f8672f5a28b68b2c8de50c3073a72
Serdar Özdemir <soezdemir3423@gmail.com>
2023-03-15 12:00:37 +0100

    Added base url to vite config for building

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import Icons from 'unplugin-icons/vite';
-import IconsResolver from 'unplugin-icons/resolver';
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/weather-app/",
   plugins: [
-    react(), 
+    react(),
     AutoImport({
       resolvers: [
         IconsResolver({
@@ -16,5 +17,6 @@ export default defineConfig({
         }),
       ],
     }),
-    Icons({compiler: 'jsx'})],
-})
+    Icons({ compiler: "jsx" }),
+  ],
+});
