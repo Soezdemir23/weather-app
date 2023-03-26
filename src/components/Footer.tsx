bbcd4905ebb108c33c8c44b4ab716b3d3ac7dc3a
Serdar Özdemir <soezdemir3423@gmail.com>
2023-03-26 03:07:51 +0200

    Reworked the  Footer
    Copied and pasted the Footer from the docs of Material Tailwind, also added necessary utility classes back necessary to keep up the sticky footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,54 @@
-export default function Footer() {
-  return <footer className="bg-white p-4 mt-auto">Footer</footer>;
+import { Typography } from "@material-tailwind/react";
+import { Temporal } from "temporal-polyfill";
+
+export default function SimpleFooter() {
+  return (
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+      <Typography color="blue-gray" className="font-normal">
+        &copy; {Temporal.Now.plainDateISO().toString()} Weather-App
+      </Typography>
+      <ul className="flex flex-wrap items-center gap-y-2 gap-x-8 list-none mt-auto">
+        <li>
+          <Typography
+            as="a"
+            href="https://github.com/Soezdemir23/weather-app"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 no-underline"
+          >
+            About This Project
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="https://github.com/Soezdemir23/weather-app/blob/master/LICENSE.txt"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 no-underline"
+          >
+            License
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="https://www.linkedin.com/in/serdaroezdemir/"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 no-underline"
+          >
+            Github Profil
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="https://www.linkedin.com/in/serdaroezdemir/"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 no-underline"
+          >
+            LinkedIn
+          </Typography>
+        </li>
+      </ul>
+    </footer>
+  );
 }
